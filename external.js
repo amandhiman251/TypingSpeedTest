27c295de16d24dddf4befd0a910395ec9210c498
Aman Dhiman <amandhiman251@gmail.com>
2020-10-16 00:42:01 +0530

    Time difference between two clicks of a button

--- external.js
+++ external.js
@@ -1,13 +1,14 @@
 var a, b, c;
+function clk() {
+	a = Date.now();
+}
 function start() {
-	 a = new Date();
-	 
+	 document.getElementById("i3").innerHTML = "Done";
+	 document.getElementById("i3").addEventListener("click", stop);
 }
 function stop() {
-	b = new Date();
+	b = Date.now();
 	c = (b - a);
-	c= (c/1000)/60;
-	
-
+	c = (c/1000)/60;
 	document.getElementById("i1").innerHTML = c;
 }
