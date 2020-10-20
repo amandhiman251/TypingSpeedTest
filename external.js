bf9b804366d3d896076cb80478d845ae95de9e3b
Aman Dhiman <amandhiman251@gmail.com>
2020-10-20 22:48:43 +0530

    final:Call stop function only once

--- external.js
+++ external.js
@@ -1,26 +1,25 @@
-let a, b, c, d, e;
+let a, b, c, d, e, f = true;
 function clk() {
 	a = Date.now();
 }
 function start() {
-	 document.getElementById("i4").addEventListener("load", getFocus);
-	 document.getElementById("i2").style.display = "block";
-	 document.getElementById("i3").innerHTML = "Done";
-	 document.getElementById("i3").addEventListener("click", stop);
+	document.getElementById("i2").style.display = "block";
+  	document.getElementById("i3").innerHTML = "Done";
+  	document.getElementById("i3").addEventListener("click", stop);
 }
 function stop() {
-	b = Date.now();
-	c = (b - a);
-	c = (c/1000)/60;
-	d = document.getElementById("i2").value.split(' ');
-	e = d.length/c;
-	e = Math.round(e);
-	if ( e < 27) {
-		document.getElementById("i1").innerHTML = "Your typing speed is " + "<b>"+ e + "</b>" + " words per minute." + "<br>" + "You need to do more Practice &#128546;";
-	}else {
-		document.getElementById("i1").innerHTML = "Your typing speed is " + "<b>"+ e + "</b>" + " words per minute." + "<br>" + "You are Good to Go." +"<br>" +"Best of Luck &#128077;";
+	if (f == true) {
+		b = Date.now();
+		c = (b - a);
+		c = (c/1000)/60;
+		d = document.getElementById("i2").value.split(' ');
+		e = d.length/c;
+		e = Math.round(e);
+		if ( e < 27) {
+			document.getElementById("i1").innerHTML = "Your typing speed is " + "<b>"+ e + "</b>" + " words per minute." + "<br>" + "You need to do more Practice &#128546;";
+		}else {
+			document.getElementById("i1").innerHTML = "Your typing speed is " + "<b>"+ e + "</b>" + " words per minute." + "<br>" + "You are Good to Go." +"<br>" +"Best of Luck &#128077;";
+		}
 	}
-}
-function getFocus(){
-	document.getElementById("i2").focus();
+		f = false;
 }
